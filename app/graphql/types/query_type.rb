0835f1e4e2b2c08ac06c154628b273af11e1ab99
Erdong Feng <fedcuit@gmail.com>
2020-01-13 22:49:30 +0800

    init commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class QueryType < Types::BaseObject
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    field :items, [Types::ItemType], null: false, description: 'Returns a list of items in martian library'
+
+    def items
+      Item.preload(:user)
+    end
+  end
+end
